1f136d6508ed2f9ca2c17ce69bb72acc4d03dfe5
Abdul Wajid <abdulwajid609@gmail.com>
2025-10-31 05:36:37 +0500

    placed the dumy script
    Removed the block of code that processes and bandwidth calculation script.

--- generlize-script.sh
+++ generlize-script.sh
@@ -52,27 +52,8 @@ _success "Server list fetched successfully."
 _note "Enter the commands to execute on all servers (end with EOF):"
 tmpfile=$(mktemp)
 cat > "$tmpfile" <<'EOF'
-for A in $(ls /etc/nginx/sites-available/ | awk '!/^default/ {print $1}'); do
-    echo "" > total.txt
-    echo "------------------------------------------------------------"
-    echo "Application: $A"
-    echo "------------------------------------------------------------"
-
-    if [[ -f "/home/master/applications/$A/conf/server.nginx" ]]; then
-        awk 'NR==1 {print substr($NF, 1, length($NF)-1)}' "/home/master/applications/$A/conf/server.nginx"
-    else
-        echo "⚠️  No server.nginx file found for $A"
-        continue
-    fi
-
-    for i in {30..0}; do
-        zcat -f "/home/master/applications/$A/logs/"*_*.access.log* 2>/dev/null \
-        | awk -v day="$(date --date="$i days ago" '+%d/%b/%Y')" \
-          '$4 ~ day {sum += $10} END {print sum >> "total.txt" ; printf("%s %.3f %s\n", day, sum/1024/1024, "MB")}'
-    done
-
-    awk '{total +=$1} END {printf ("%s %.3f %s\n", "Total:", total/1024/1024/1024, "GB")}' total.txt
-done
+# test script
+ls -la 
 
 EOF
 
